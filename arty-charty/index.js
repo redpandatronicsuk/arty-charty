18f1b96dc5d0fcc47f53cb672aa7efd410f1ddcc
RedPandaTronics <dobe@Dominiks-MacBook-Pro-2.local>
2017-01-23 19:57:02 +0000

    Heatmap

--- arty-charty/index.js
+++ arty-charty/index.js
@@ -3,6 +3,7 @@ export { default as ArtyChartyXY } from './arty-charty-xy';
 export { default as AmimatedCirclesMarker } from './animated-circles-marker';
 export { default as ArtyChartyDonut } from './arty-charty-donut';
 export { default as ArtyChartyPie } from './arty-charty-pie';
+export { default as ArtyChartyHeatmap } from './arty-charty-heatmap';
 export { default as ArtySparkyLine } from './arty-sparky-line';
 export { default as ArtySparkyPie } from './arty-sparky-pie';
 export { default as Tweener } from './tweener';
